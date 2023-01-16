53675e509408f67015905a0a3ae9eef41c0d6d36
Alex Sander <alexsander01@hotmail.com.br>
2023-01-16 20:56:30 -0300

    fix: correção na tag

--- bibli_films/src/components/navbar/navbar.components.jsx
+++ bibli_films/src/components/navbar/navbar.components.jsx
@@ -1,21 +1,24 @@
 import { Link } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai   '
 import Home from '../../pages/Home'
+import './navbar.css'
 
 const NavBar = () => {
   return (
-    <>
-    <navbar id="navbar">
+    <div>
+      <div id="navbar">
         <h2>
           <Link to='/'>MoviesLib</Link>
         </h2>
-      </navbar>
         <form>
             <input type="text" placeholder='Busque um filme' />
             <button type='submit'><AiOutlineSearch/></button>
         </form>
-        <Home/>
-    </>
+      </div>
+        <Home/> 
+    </div>
+    
+    
   )
 }
 
