f49fd8d084c95cb7fbe4451969faeb8e3f4cb564
Alex Sander <alexsander01@hotmail.com.br>
2023-01-23 20:03:10 -0300

    feat: add Outlet

--- bibli_films/src/App.jsx
+++ bibli_films/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/navbar/navbar.components'
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Outlet/>
 
     </div>
   )
